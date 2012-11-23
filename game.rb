6b10f006baca64156bcb007f923852cc1389d50d
Lars Pettersson <lars.pettersson@elev.it-gymnasiet.se>
2012-11-23 10:24:17 +0100

    Added player class

--- game.rb
+++ game.rb
@@ -5,6 +5,15 @@ class Game < Chingu::Window
 	def initialize
 		super
 		self.input = {esc: :exit}
+		@player = Player.create
+	end
+end
+
+class Player < Chingu::GameObject
+
+	def setup
+		@image = Gosu::Image["skepp.png"]
+		@x, @y = 350, 400
 	end
 end
 
