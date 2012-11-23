cbde3840476f49082352efc8ce3ee07bdc10f23e
Lars Pettersson <lars.pettersson@elev.it-gymnasiet.se>
2012-11-23 10:39:06 +0100

    Added player movement

--- game.rb
+++ game.rb
@@ -3,17 +3,44 @@ require 'chingu'
 class Game < Chingu::Window
 
 	def initialize
-		super
+		super(640,480,false)
 		self.input = {esc: :exit}
 		@player = Player.create
 	end
 end
+
 
 class Player < Chingu::GameObject
 
 	def setup
 		@image = Gosu::Image["skepp.png"]
 		@x, @y = 350, 400
+		self.input = {
+			holding_left: :left,
+			holding_right: :right,
+			holding_up: :up,
+			holding_down: :down
+		}
+	end
+	def left
+		unless @x < 15
+			@x -= 3.3
+		end
+	end
+	def right
+		unless @x > 625
+			@x += 3.3
+		end
+	end
+	def up
+		unless @y < 100
+			@y -= 2.5
+		end
+	end
+	def down
+		unless @y > 430
+			@y += 2.5
+		end
 	end
 end
 
